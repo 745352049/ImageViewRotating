007d3990bf59f0d3b9c10fe80ba0604cedf0b556
iOS开发 <ioskaifa@iMac.local>
2020-06-24 15:30:24 +0800

    Add YTRotatingView

--- ImageViewRotating/ViewController.m
+++ ImageViewRotating/ViewController.m
@@ -10,9 +10,12 @@
 
 #import "YTRotatingImageView.h"
 
+#import "YTRotatingView.h"
+
 @interface ViewController ()
 
 @property (nonatomic, strong) YTRotatingImageView *imageV;
+@property (nonatomic, strong) YTRotatingView *rotateView;
 
 @end
 
@@ -22,11 +25,17 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.imageV.frame = CGRectMake(100, 100, 200, 200);
+    self.imageV.frame = CGRectMake(100, 40, 200, 200);
     self.imageV.layer.cornerRadius = 100;
     [self.view addSubview:self.imageV];
     self.imageV.isResetWithResume = YES;
     [self.imageV startRotatingWithAngle:M_PI*2.0 Duration:5.0 RepeatCount:MAXFLOAT];
+    
+    [self.view addSubview:self.rotateView];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Image"]];
+    imageView.frame = self.rotateView.bounds;
+    [self.rotateView addSubview:imageView];
+    [self.rotateView startRotatingWithAngle:M_PI*2.0 Duration:4.0 RepeatCount:5];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -37,18 +46,32 @@
 
 - (IBAction)pause:(UIButton *)sender {
     [self.imageV pauseRotating];
+    [self.rotateView pauseRotating];
 }
 
 - (IBAction)resume:(UIButton *)sender {
     [self.imageV resumeRotating];
+    [self.rotateView resumeRotating];
 }
 
 - (IBAction)stop:(UIButton *)sender {
     [self.imageV stopRotating];
+    [self.rotateView stopRotating];
 }
 
 - (IBAction)start:(UIButton *)sender {
     [self.imageV startRotating];
+    [self.rotateView startRotating];
+}
+
+- (YTRotatingView *)rotateView {
+    if (!_rotateView) {
+        _rotateView = [[YTRotatingView alloc] initWithFrame:CGRectMake(100, CGRectGetMaxY(self.imageV.frame) + 20, 100, 100)];
+        _rotateView.backgroundColor = [UIColor blueColor];
+        _rotateView.layer.cornerRadius = 50;
+        _rotateView.clipsToBounds = YES;
+    }
+    return _rotateView;
 }
 
 - (YTRotatingImageView *)imageV {
